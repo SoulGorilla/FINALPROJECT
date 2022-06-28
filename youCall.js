ce7b6478415d06bf9767d28596a311b9c70a9c7d
Cobb <cobb@thecobralounge.com>
2022-06-28 09:18:18 -0700

    Second Commit- pushing to Github

---
+++ youCall.js
@@ -0,0 +1,7 @@
+
+// Take user input and append to an api list search
+// list returns videoID's and Channel Id's
+//API call for video Id's number of views
+//API call for channel number of subscribers
+//divide number of views by number of subscribers 
+// sort by this number 
